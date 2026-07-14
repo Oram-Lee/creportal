e98483dddccf10602414ff244adbe4d993999df2
Oram-Lee <oramlee@sni.co.kr>
2026-07-14 14:55:14 +0900

    6th fix

--- js/mreport-main.js
+++ js/mreport-main.js
@@ -6,8 +6,8 @@
 import {
   buildDraftModel, loadModel, saveModel,
   generateAiDraft, mergeAiDraft, quarterLabel,
-} from './mreport-data.js?v=1.1.2';
-import { renderReport, collectModel } from './mreport-render.js?v=1.1.2';
+} from './mreport-data.js?v=1.1.4';
+import { renderReport, collectModel } from './mreport-render.js?v=1.1.4';
 
 const $ = sel => document.querySelector(sel);
 
